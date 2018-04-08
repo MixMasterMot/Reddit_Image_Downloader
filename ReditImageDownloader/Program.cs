ffbaf06eebcff49f1be4fddd974855be228bcde2
Louis Botha <ljbotha93@gmail.com>
2018-04-08 21:08:25 +0200

    Add files via upload

--- ReditImageDownloader/Program.cs
+++ ReditImageDownloader/Program.cs
@@ -7,6 +7,8 @@ using Newtonsoft.Json.Linq;
 using System.IO;
 using RedditSharp;
 using RedditSharp.Things;
+using System.Diagnostics;
+using System.Threading;
 
 namespace ReditImageDownloader
 {
@@ -24,6 +26,7 @@ namespace ReditImageDownloader
 
             prog.RemoveOldImage(saveLocation);
             List<string> subReddits = prog.GetSubReddit();
+            List<int> targetResolution = prog.GetTargetResolution();
 
             Reddit reddit = new Reddit();
             
@@ -42,10 +45,26 @@ namespace ReditImageDownloader
                     string postURL = Convert.ToString(post.Url);
                     string postName = Convert.ToString(post.Title);
                     postName = prog.MakeNameSafe(postName);
+
                     if (!prog.ValidImgUrl(postURL))
                     {
                         continue;
                     }
+
+                    var func = new Func<bool>(() =>
+                    {
+                        return prog.CheckImageSize(targetResolution[0], targetResolution[1], postURL);
+                    });
+                    bool goodSize;
+                    TryExecute(func,500, out goodSize);
+
+                    //bool goodSize = prog.CheckImageSize(targetResolution[0], targetResolution[1], postURL);
+
+                    if (goodSize == false)
+                    {
+                        continue;
+                    }
+
                     string img = prog.DownloadImages(postURL, saveLocation, postName);
                     if (img != null)
                     {
@@ -78,7 +97,7 @@ namespace ReditImageDownloader
                 string tmp = null;
                 for(int i = 0;i<6;i++)
                 {
-                    tmp = tmp + splitName[0] + " ";
+                    tmp = tmp + splitName[i] + " ";
                 }
                 postName = tmp;
             }
@@ -97,8 +116,21 @@ namespace ReditImageDownloader
             {
                 subs.Add(pair.Value);
             }
-
             return subs;
+        }
+        private List<int> GetTargetResolution()
+        {
+            Dictionary<string, string> cmds;
+            string json = File.ReadAllText("SubReddits/Resolution.json");
+            var data = JsonConvert.DeserializeObject<dynamic>(json);
+            cmds = data.ToObject<Dictionary<string, string>>();
+
+            List<int> values = new List<int>();
+            foreach (KeyValuePair<string, string> pair in cmds)
+            {
+                values.Add(Convert.ToInt32(pair.Value));
+            }
+            return values;
         }
         private string GetSaveDir()
         {
@@ -216,6 +248,45 @@ namespace ReditImageDownloader
             Console.ForegroundColor = colorFront;
             Console.WriteLine(text); 
             Console.ResetColor();
+        }
+
+        public bool CheckImageSize(int targetX,int targetY,string targetUrl)
+        {
+            bool retTuple = true;
+            try
+            {
+                Uri uri = new Uri(targetUrl);
+                Size size = GetImageResolution.GetWebDimensions(uri);
+                if (size.xSize < targetX && size.ySize < targetY)
+                {
+                    retTuple = false;
+                }
+            }
+            catch
+            {
+                retTuple = false;
+            }
+            return retTuple;
+        }
+        public static T Execute<T>(Func<T> func, int timeout)
+        {
+            T result;
+            TryExecute(func, timeout, out result);
+            return result;
+        }
+
+        public static bool TryExecute<T>(Func<T> func, int timeout, out T result)
+        {
+            var t = default(T);
+            var thread = new Thread(() => t = func());
+            thread.Start();
+            var completed = thread.Join(timeout);
+            if (!completed)
+            {
+                thread.Abort();
+            } 
+            result = t;
+            return completed;
         }
 
     }
